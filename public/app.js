8636ee796d19983678505ef856f6598c0935d20a
vahidrza <vrzayev79@mail.com>
2023-07-10 02:25:47 +0400

    Log in started

--- public/app.js
+++ public/app.js
@@ -1,28 +1,28 @@
 const callProcedureBtn = document.getElementById("callProcedureBtn");
 
-      callProcedureBtn.addEventListener("click", (e) => {
-        e.preventDefault();
-        let data = [];
-        let elements = document.getElementsByClassName("formItem");
+callProcedureBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let data = [];
+  let elements = document.getElementsByClassName("formItem");
 
-        for (let index = 0; index < 7; index++) {
-          data[index] = elements[index].value;
-        }
+  for (let index = 0; index < 7; index++) {
+    data[index] = elements[index].value;
+  }
 
-        fetch("/call-procedure", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ data }),
-        })
-          .then((response) => response.text())
-          .then((result) => {
-            console.log(result);
-            alert(result);
-          })
-          .catch((error) => {
-            console.error(error);
-            alert("Error occured during calling Procedure.");
-          });
-      });
+  fetch("/call-procedure", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ data }),
+  })
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result);
+      alert(result);
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("Error occured during calling Procedure.");
+    });
+});
