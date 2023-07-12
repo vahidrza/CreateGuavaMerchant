be9d6dd915ef12f83fb2e3e30324e91e73c5c9b0
vahidrza <vrzayev79@gmail.com>
2023-07-12 16:48:13 +0400

    changed port number

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const oracledb = require("oracledb");
 const app = express();
-const port = 3000;
+const port = 80;
 
 // Function to open connection to DB
 async function createOraclePool(dbConfig) {
