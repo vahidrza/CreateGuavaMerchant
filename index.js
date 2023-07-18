765b454eb59230b0eb7ddb699cee1149cff021e0
vahidrza <vrzayev79@gmail.com>
2023-07-18 10:03:52 +0400

    Imported package to logging

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const express = require("express");
 const oracledb = require("oracledb");
 const app = express();
+const winston = require('winston');
 const port = 3000;
+
+//Imported package to logging
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'app.log' })
+  ]
+});
+
 
 // Function to open connection to DB
 async function createOraclePool(dbConfig,data) {
   try {
     const pool = await oracledb.createPool(dbConfig);
     console.log(new Date() + "Pool opened.");
+    logger.info(new Date() + "Pool opened.");
     setTimeout(() => {
       pool.close();
       console.log(new Date() + 'Pool successfully Closed');
+      logger.info(new Date() + 'Pool successfully Closed');
      }, 5000);
   } catch (err) {
     console.error(new Date() + "Error occured while connecting DB:", err);
+    logger.error(new Date() + "Error occured while connecting DB:", err);
     process.exit(1);
     
   }
   console.log(new Date() + "Entered Credentials: "+ JSON.stringify(dbConfig));
+  logger.info(new Date() + "Entered Credentials: "+ JSON.stringify(dbConfig));
   console.log(new Date() + "Entered Data: "+ JSON.stringify(data));
+  logger.info(new Date() + "Entered Data: "+ JSON.stringify(data));
 }
 
 // Use public folder to serve static files.
@@ -29,6 +45,7 @@ app.use(express.json());
 app.post("/call-procedure", async (req, res) => {
   //Starting to power on the Function (Connecting to DB)
   console.log(new Date() + 'createOraclePool function called');
+  logger.info(new Date() + 'createOraclePool function called');
   await createOraclePool(req.body.requestBody.dbConfig,req.body.requestBody.data);
   let connection;
 
@@ -53,15 +70,17 @@ app.post("/call-procedure", async (req, res) => {
     res.status(200).send("Procedure successfully called and committed.");
   } catch (err) {
     console.error(new Date() + "Error occured during calling Procedure:", err);
+    logger.error(new Date() + "Error occured during calling Procedure:", err);
     res.status(500).send("Error:" + err);
   } finally {
     if (connection) {
       try {
         await connection.close();
-        
         console.log(new Date() + "Oracle DB connection successfully closed");
+        logger.info(new Date() + "Oracle DB connection successfully closed");
       } catch (err) {
         console.error(new Date() + "Oracle DB connection couldn't closed:", err);
+        logger.error(new Date() + "Oracle DB connection couldn't closed:", err);
       }
     }
   }
@@ -70,4 +89,5 @@ app.post("/call-procedure", async (req, res) => {
 // Starting the server
 app.listen(port, '0.0.0.0' , () => {
   console.log(new Date() + `Server is running at ${port}:th port 🚀🚀🚀.`);
+  logger.info(new Date() + `Server is running at ${port}:th port 🚀🚀🚀.`);
 });
